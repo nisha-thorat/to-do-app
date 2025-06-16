a3ebad69a6e01278bda78313532e6453ef405a93
nisha-thorat <nishathorat123455@gmail.com>
2025-06-16 08:41:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,60 @@
+const todoInput = document.getElementById("todo-input");
+const todosContainer = document.getElementById("todos");
+
+
+let todos = [];
+
+
 function addTodo() {
-      const input = document.getElementById('todo-input');
-      const text = input.value;
+  const text = todoInput.value.trim(); 
 
-      if (text === "") {
-        alert("Please enter a todo.");
-        return;
-      }
-
-      const todoDiv = document.createElement('div');
-      todoDiv.className = "todo-item";
-
-      const para = document.createElement('p');
-      para.textContent = text;
-      
-
-      const deletebutton=document.createElement('button');
-      deletebutton.innerHTML="Delete";
-      deletebutton.className="deletebtn";
-      deletebutton.onclick=function(){
-        todoDiv.remove();
-      };
-      
-      todoDiv.appendChild(para);
-      todoDiv.appendChild(deletebutton);
+  if (text !== "") {
+    
+    todos.push({ text: text, completed: false });
+    todoInput.value = ""; 
+    showTodos(); 
+  }
+}
 
 
-     document.getElementById('todo-list').appendChild(todoDiv);
+function showTodos() {
+  todosContainer.innerHTML = "";
 
-      input.value="";
+  
+  for (let i = 0; i < todos.length; i++) {
+    const todoItem = document.createElement("div");
+    todoItem.className = "todo-item";
+
+    
+    if (todos[i].completed) {
+      todoItem.classList.add("completed");
     }
+
+    const para = document.createElement("p");
+    para.innerText = todos[i].text;
+
+    
+    const markBtn = document.createElement("button");
+    markBtn.className="markBtn";
+    markBtn.innerText = todos[i].completed ? "Unmark" : "Mark";
+    markBtn.onclick = function () {
+      todos[i].completed = !todos[i].completed; 
+      showTodos();
+    };
+
+    
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Delete";
+    deleteBtn.className="deleteBtn";
+    deleteBtn.onclick = function () {
+      todos.splice(i, 1); 
+      showTodos();
+    };
+
+    
+    todoItem.append(para, markBtn, deleteBtn);
+    todosContainer.appendChild(todoItem);
+  }
+}
+
+      
